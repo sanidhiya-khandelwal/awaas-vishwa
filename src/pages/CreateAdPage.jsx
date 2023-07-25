64029a730ca79fe20166733014687f79bbfd14f5
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2023-07-25 17:47:08 +0530

    First Commit🚀

---
+++ src/pages/CreateAdPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreateAdPage = () => {
+    return (
+        <div>CreateAdPage</div>
+    )
+}
+
+export default CreateAdPage
---
+++ src/pages/EditAdPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EditAdPage = () => {
+    return (
+        <div>EditAdPage</div>
+    )
+}
+
+export default EditAdPage
